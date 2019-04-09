cebc5e292ba950601e2e6c7e77048af5ff2aaefc
rmayer <cassettes90@gmail.com>
2019-04-08 23:00:04 -0300

    updates for vpid

--- node/main.js
+++ node/main.js
@@ -1,10 +1,10 @@
 const webPush = require('web-push');
 
-const pushSubscription = {"endpoint":"https://android.googleapis.com/gcm/send/eqZ_KcFtVEU:APA91bFPCFAwe9_MttlVBGWFtD4oW8hcGlENOSPNf2cZ81zmqy-IUkNlS2CgTwJWb8UkPsAlVzWJLpQXD5rKTPknMscMR8mRAJ3GsvFgg5nJu-A1zg11PaZM7G7U0uyD6rpixolrP0oa","expirationTime":null,"keys":{"p256dh":"BKrN1WBgAnYF3KV9OFAk6bJVO_tuoFJuC-f-jDUzqJ8yTHGasjPVb-C5FZkgbHTQLwAJPJxaUpGnq9mMRiL4JLE","auth":"ZqUIcQpf1GjgSzaUQLz1hw"}};
+const pushSubscription = {"endpoint":"https://android.googleapis.com/gcm/send/eODf-1W93QQ:APA91bFbJqkFkcRDG3UUJaAlmorZx_W_10THqKp223G0-jhOwORETyUp_nt-6rAqrxTdJYUb7LnO5p5qgY48eDe81j2okxSAyKVyyQNZY84M7pQAIJAu8INGEmX82X5ekyzCRd8AJY-V","expirationTime":null,"keys":{"p256dh":"BONgssu9tc2MbLYNYGb7gitK9QxoRt4A7X9iUnky9cN2ydaJJjL2y5N95GNkfiwrN6fso2Zo16qGKne8iGGWGtM","auth":"nem1kmdV6X7wEZfQd24Byg"}};
 
 // TODO 4.3a - include VAPID keys
 
-const payload = 'Here is a payload!';
+const payload = 'Check it out!';
 
 const options = {
   gcmAPIKey: 'AAAAGs7Nzao:APA91bHS-oqSGeX8yAJZpzRYvWuXmoXxOgYVv1rsp16IU6sDdviTf0nrmAd31tsSao62kdD1RUhiKFvzcGAag39HMHZOPcDBW-0pSggQJSiizSxkcH32gd0wfFnsjXK5UVtMiLa9jDIf',
